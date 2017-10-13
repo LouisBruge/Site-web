8582c664714f0e4be2d708c5143df001bc6fbadf
griselangue <louis.bruge@gmail.com>
2017-10-13 19:46:23 +0200

    Modified design.css in order to used flexbox

--- index.php
+++ index.php
@@ -11,32 +11,38 @@ session_start();
 			<link rel="stylesheet" href="design.css" type="text/css" media="screen" />
 		</head>
 		<body>
+		<header>
+			<h1> Home's Server </h1>
+		</header>
+
 			<?php include("menu.php") ; ?>
-		<section>
+		<main>
 
 			<h1> Bienvenue sur le site du réseau local de la maison</h1>
-            <p> Le site a pour vocation d'améliorer la gestion interne du pc (serveur de musique) ainsi que des différentes bases de données liées à ce dernier (bibliographiques, lithiques et référencement des sites archéologiques et des opérateurs)<br/>
-        <strong> Le site est pour le moment en construction et sa structure est susceptible d'être modifier à l'avenir</strong></p>
-
-		<fieldset> 
-		<legend> Base multimédia </legend>
-		<p> <h2>Base de donnée biblio</h2>
-		Accés au formulaire d'enregistrement de la base bibliographique : <a href="formulaires.php#monographie"> ici </a><br/>
-		Accés à la base bibliographique : <a href="SQL/ouvrage.php"> ici </a><br/>
+	    	<p> 
+			Le site a pour vocation d'améliorer la gestion interne du pc (serveur de musique) ainsi que des différentes bases de données liées à ce dernier (bibliographiques, lithiques et référencement des sites archéologiques et des opérateurs)<br/>
+	<strong> Le site est pour le moment en construction et sa structure est susceptible d'être modifier à l'avenir</strong>
 		</p>
 
-		<p> <h2> Base de donnée des jeux vidéo </h2>
-		Accés au formulaire d'enregistrement de la base de donnée des jeux videos : <a href="formulaires.php#jeux_video"> ici </a><br />
-		Accés à la base jeux vidéoludique : <a href="SQL/jeux_video.php"> ici </a><br/>
+		<h2>Base de donnée biblio</h2>
+		<p>
+			Accés au formulaire d'enregistrement de la base bibliographique : <a href="formulaires.php#monographie"> ici </a><br/>
+			Accés à la base bibliographique : <a href="SQL/ouvrage.php"> ici </a><br/>
 		</p>
 
-		<p> <h2> Base de donnée des films </h2>
-		Accés au formulaire d'enregistrement de la base filmographique : <a href="formulaires.php#film"> ici </a><br />
-		Accés à la base filmographique : <a href="SQL/film.php"> ici </a><br/>
+		 <h2> Base de donnée des jeux vidéo </h2>
+		<p>
+			Accés au formulaire d'enregistrement de la base de donnée des jeux videos : <a href="formulaires.php#jeux_video"> ici </a><br />
+			Accés à la base jeux vidéoludique : <a href="SQL/jeux_video.php"> ici </a><br/>
 		</p>
 
-		</fieldset>
-		</section>
+		<h2> Base de donnée des films </h2>
+		<p>
+			Accés au formulaire d'enregistrement de la base filmographique : <a href="formulaires.php#film"> ici </a><br />
+			Accés à la base filmographique : <a href="SQL/film.php"> ici </a><br/>
+		</p>
+
+		</main>
 		</body>
 			<?php include("footer.php") ; ?>
 	</html>
