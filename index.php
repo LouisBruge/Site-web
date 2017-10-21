790dcabbd832434ea2deb425ee4dccce836c81e9
louis.bruge <louis.bruge@gmail.com>
2017-10-21 12:30:11 +0000

    Rewriting of the index.php's structure for modular infrastructure

--- index.php
+++ index.php
@@ -12,31 +12,40 @@ session_start();
 		</head>
 		<body>
 			<?php include("menu.php") ; ?>
-				<section>
+				<section class="accesDB">
 
-				<h1> Bienvenue sur le site du réseau local de la maison</h1>
+                <section ="banner">
+				    <h1> Bienvenue sur le site du réseau local de la maison</h1>
 	    			<p> 
-					Le site a pour vocation d'améliorer la gestion interne du pc (serveur de musique) ainsi que des différentes bases de données liées à ce dernier (bibliographiques, lithiques et référencement des sites archéologiques et des opérateurs)<br/>
-					<strong> Le site est pour le moment en construction et sa structure est susceptible d'être modifier à l'avenir</strong>
-				</p>
+					    Le site a pour vocation d'améliorer la gestion interne du pc (serveur de musique) ainsi que des différentes bases de données liées à ce dernier (bibliographiques, lithiques et référencement des sites archéologiques et des opérateurs)<br/>
+					    <strong> Le site est pour le moment en construction. Sa structure est susceptible d'évoluer à l'avenir</strong>
+				    </p>
+                </section>
 
-				<h2>Base de donnée biblio</h2>
-				<p>
-					Accés au formulaire d'enregistrement de la base bibliographique : <a href="formulaires.php#monographie"> ici </a><br/>
-					Accés à la base bibliographique : <a href="SQL/ouvrage.php"> ici </a><br/>
-				</p>
+                <div class="biblio">
+				    <h2>Base de donnée biblio</h2>
+				    <p>
+				    	<a href="SQL/ouvrage.php">Accés à la base bibliographique</a><br/>
+				    	<a href="formulaires.php#monographie">Accés au formulaire d'enregistrement de la base bibliographique</a><br/>
+				    </p>
+                </div>
 
-				 <h2> Base de donnée des jeux vidéo </h2>
-				<p>
-					Accés au formulaire d'enregistrement de la base de donnée des jeux videos : <a href="formulaires.php#jeux_video"> ici </a><br />
-					Accés à la base jeux vidéoludique : <a href="SQL/jeux_video.php"> ici </a><br/>
-				</p>
 
-				<h2> Base de donnée des films </h2>
-				<p>
-					Accés au formulaire d'enregistrement de la base filmographique : <a href="formulaires.php#film"> ici </a><br />
-					Accés à la base filmographique : <a href="SQL/film.php"> ici </a><br/>
-				</p>
+                <div class="jeuxVideo">
+				    <h2> Base de donnée des jeux vidéo </h2>
+				    <p>
+					    <a href="SQL/jeux_video.php">Accés à la base jeux vidéoludique</a><br/>
+					    <a href="formulaires.php#jeux_video">Accés au formulaire d'enregistrement de la base de donnée des jeux videos</a><br />
+				    </p>
+                </div>
+
+                <div class="film">
+				    <h2> Base de donnée des films </h2>
+				    <p>
+					    <a href="SQL/film.php">Accés à la base filmographique</a><br/>
+					    <a href="formulaires.php#film">Accés au formulaire d'enregistrement de la base filmographique</a><br />
+				    </p>
+                </div>
 				</section>
 			</main>
 		</body>
