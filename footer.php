0f45e1735dacaa2956d8d279649f697ca0ea9d05
griselangue <louis.bruge@gmail.com>
2017-10-23 12:06:49 +0200

    Remastering design.css for white and black design

--- footer.php
+++ footer.php
@@ -1,9 +1,15 @@
 <footer>
-<p> © L. Bruge 2016 - 2017 
-php : <?php echo phpversion(); ?> et postgresql : 9.6.1
-	<?php require($_SERVER['DOCUMENT_ROOT'].'/Login/ConnectionDB.php');
-	echo $db->query('SELECT version()') ->fetchColumn(); 
-	?>
-</p>
+	<div class="credits">
+		<p> © L. Bruge 2016 - 2017 </p>
+	</div>
+	<div class="powered">
+		<p>
+			php : <?php echo phpversion(); ?>
+			 et postgresql : 9.6.1
+			<?php require($_SERVER['DOCUMENT_ROOT'].'/Login/ConnectionDB.php');
+			echo $db->query('SELECT version()') ->fetchColumn(); 
+			?>
+		</p>
+	</div>
 </footer>
 	
