d9a4b231f3fb444b67c26269637a30974e7c74b9
louis.bruge <louis.bruge@gmail.com>
2017-10-26 07:59:56 +0000

    move fields under labels in Annuaire/formulaire.php

--- Annuaire/formulaire.php
+++ Annuaire/formulaire.php
@@ -18,25 +18,52 @@
 				<form method="post" action="/Annuaire/annuaire.php" id="annuaire">
 				<fieldset>
 				<legend> Etat civil </legend>
-					Nom : <input type="text" name="nom" size=60 placeholder="Nom"required /> <br />
-					Prenom : <input type="text" name="prenom" placeholder="Prenom" required /><br />
-					Date de naissance : <input type="date" name="naissance" placeholder="JJ/MM/AAAA" /><br />
-					Date de décès : <input type="date" name="mort" placheholder="JJ/MM/AAAA" /><br />
+                    Nom : <br /> 
+                    <input type="text" name="nom" size=60 placeholder="Nom"required /> <br />
+
+                    Prenom : <br />
+                    <input type="text" name="prenom" placeholder="Prenom" required /><br />
+
+                    Date de naissance : <br />
+                    <input type="date" name="naissance" placeholder="JJ/MM/AAAA" /><br />
+
+                    Date de décès : <br />
+                    <input type="date" name="mort" placheholder="JJ/MM/AAAA" /><br />
+
 				</fieldset>
 				<fieldset>
 				<legend> Coordonnées </legend>
-					Bâtiment ou villa... : <input type="text" name="batiment" /> <br />
-					Numéro de rue : <input type="text" name="numero_rue" /> <br />
-					Rue : <input type="text" name="rue" /> <br />
-					Complément : <input type="text" name="complement" /> <br />
-					Code postal : <input type="number" name="code_postal" /><br />
-					Ville : <input type="text" name="ville" /> <br />
-					Cedex : <input type="text" name="cedex" placeholder="CEDEX 14"/><br />
+                    Bâtiment ou villa... : <br />
+                    <input type="text" name="batiment" /> <br />
+
+                    Numéro de rue : <br />
+                    <input type="text" name="numero_rue" /> <br />
+
+                    Rue : <br />
+                    <input type="text" name="rue" /> <br />
+
+                    Complément : <br />
+                    <input type="text" name="complement" /> <br />
+
+                    Code postal : <br />
+                    <input type="number" name="code_postal" /><br />
+
+                    Ville : <br />
+                    <input type="text" name="ville" /> <br />
+
+                    Cedex : <br />
+                    <input type="text" name="cedex" placeholder="CEDEX 14"/><br />
 
 					<br />
-					Téléphone mobile : <input type="text" name="mobile" placeholder="XXXXXXXX" /><br />
-					Téléphone fixe : <input type="text" name="fixe" placeholder="XXXXXXXX" /><br />
-					Mail : <input type="mail" name="mail" placeholder="xxx.xxx@xxx.xxx" /> <br />
+                    Téléphone mobile : <br />
+                    <input type="text" name="mobile" placeholder="XXXXXXXX" /><br />
+
+                    Téléphone fixe : <br />
+                    <input type="text" name="fixe" placeholder="XXXXXXXX" /><br />
+
+                    Mail : <br />
+                    <input type="mail" name="mail" placeholder="xxx.xxx@xxx.xxx" /> <br />
+
 				</fieldset>
 				<input type="submit" name="validation" />
 				</form>
