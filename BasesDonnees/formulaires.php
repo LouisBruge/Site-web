e335467147fd362008123b852c203409f46dc877
louis.bruge <louis.bruge@gmail.com>
2017-10-26 21:03:15 +0000

    Move from a Z-style to a underline style formulaire

--- BasesDonnees/formulaires.php
+++ BasesDonnees/formulaires.php
@@ -20,13 +20,26 @@
  	       	<form method="post" action="/BasesDonnees/ENREG/ouvrage.php" id="monographie">
         	<fieldset>
         	<legend> Ouvrage </legend>
-        	Auteur(s) : <input type="text" name="auteur" size=60 required/> <br/>
-	        Titre : <input type="text" name="titre" size=60 required/> <br/>
-       		Date : <input type="number" name="date" required/> <br/> 
-      		Editeur : <input type="text" name="editeur" required/> <br/>
-	        Ville d'édition : <input type="text" name="ville" required/> <br/>
-	        Collection : <input type="text" name="collection" /> <br />
-            	Commentaire : <textarea name="commentaire" rows="2" cols="45" ></textarea> <br />
+            Auteur(s) : <br />
+            <input type="text" name="auteur" size=60 required/> <br/>
+
+            Titre : <br />
+            <input type="text" name="titre" size=60 required/> <br/>
+
+            Date : <br />
+            <input type="number" name="date" required/> <br/> 
+
+            Editeur : <br />
+            <input type="text" name="editeur" required/> <br/>
+
+            Ville d'édition : <br />
+            <input type="text" name="ville" required/> <br/>
+            Collection : <br />
+            <input type="text" name="collection" /> <br />
+
+            Commentaire : <br />
+            <textarea name="commentaire" rows="2" cols="45" ></textarea> <br />
+
 		<?php
 			info_complementaire($db, "ouvrage");// génére les listes déroulantes pour le genre, le type de support et le propriétaire
 		?>
@@ -38,12 +51,24 @@
 		<form method="post" action="/BasesDonnees/ENREG/film.php" id="film">
 		<fieldset>
 		<legend> Films </legend>
-		Titre : <input type="text" name="titre" required/> <br />
-		Réalisateur : <input type="text" name="realisateur" required/> <br />
-		Studio : <input type="text" name="studio" required/> <br />
-		Année : <input type="number" name="annee" maxlenght="4" min="1900" required="yes">
-		Durée (min) : <input type="number" name="duree" required> <br />
-		Commentaire : <textarea name="commentaire" rows="2" cols="45"> </textarea> <br />
+        Titre : <br />
+        <input type="text" name="titre" required/> <br />
+
+        Réalisateur : <br />
+        <input type="text" name="realisateur" required/> <br />
+
+        Studio : <br />
+        <input type="text" name="studio" required/> <br />
+
+        Année : <br />
+        <input type="number" name="annee" maxlenght="4" min="1900" required="yes">
+
+        Durée (min) : <br />
+        <input type="number" name="duree" required> <br />
+
+        Commentaire : <br />
+        <textarea name="commentaire" rows="2" cols="45"> </textarea> <br />
+
 		<?php
 			info_complementaire($db, "film"); // génére les listes déroulantes pour le genre, le type de support et le propriétaire
 		?>
@@ -55,10 +80,17 @@
 		<form method="post" action="/BasesDonnees/ENREG/jeux_video.php" id="jeux_video">
 		<fieldset>
 		<legend> Jeux </legend>
-		Titre : <input type="text" name="titre"  required /> <br />
-		Éditeur : <input type="text" name="editeur" required /> <br />
-		Année : <input type="number" name="annee" maxlenght="4" min="1900" required="no">
-		Plateforme : <select name="plateforme">
+        Titre : <br />
+        <input type="text" name="titre"  required /> <br />
+
+        Éditeur : <br />
+        <input type="text" name="editeur" required /> <br />
+
+        Année : <br />
+        <input type="number" name="annee" maxlenght="4" min="1900" required="no">
+
+        Plateforme : <br />
+            <select name="plateforme">
 			<option value="PC">PC</option>
 			<option value="PS2">PS2</option>
 			<option value="PS1">PS1</option>
@@ -66,7 +98,10 @@
 			<option value="Gameboy">Gameboy</option>
 			<option value="Gameboy Color">Gameboy Color</option>
 			</select> <br />
-		Commentaire : <textarea name="commentaire" rows="2" cols="45"> </textarea> <br />
+
+        Commentaire : <br />
+        <textarea name="commentaire" rows="2" cols="45"> </textarea> <br />
+
 		<?php
 			info_complementaire($db, "jeux");// génére les listes déroulantes pour le genre, le type de support et le propriétaire
 		?>
@@ -78,11 +113,20 @@
 		<form method="post" action="/BasesDonnees/ENREG/genre.php" id="genre">
 		<fieldset>
 		<legend> Genre </legend>
-		Nom : <input type="text" name="nom" required /> <br />
-		Abreviation : <input type="text" name="abreviation" required /> <br />
-		film :  <input type="radio" name="film" value="TRUE" id="oui" checked="checked" /><label for="oui">oui</label> <input type="radio" name="film" value="FALSE" id="non"/><label for="non">non</label> <br />
-		ouvrage :  <input type="radio" name="ouvrage" value="TRUE" id="oui" checked="checked" /><label for="oui">oui</label> <input type="radio" name="ouvrage" value="FALSE" id="non"/><label for="non">non</label> <br />
-		jeux :  <input type="radio" name="jeux" value="TRUE" id="oui" checked="checked" /><label for="oui">oui</label> <input type="radio" name="jeux" value="FALSE" id="non"/><label for="non">non</label> <br />
+        Nom : <br />
+        <input type="text" name="nom" required /> <br />
+
+        Abreviation : <br />
+        <input type="text" name="abreviation" required /> <br />
+    
+        film :  <br />
+        <input type="radio" name="film" value="TRUE" id="oui" checked="checked" /><label for="oui">oui</label> <input type="radio" name="film" value="FALSE" id="non"/><label for="non">non</label> <br />
+
+        ouvrage :  <br />
+        <input type="radio" name="ouvrage" value="TRUE" id="oui" checked="checked" /><label for="oui">oui</label> <input type="radio" name="ouvrage" value="FALSE" id="non"/><label for="non">non</label> <br />
+
+        jeux :  <br />
+        <input type="radio" name="jeux" value="TRUE" id="oui" checked="checked" /><label for="oui">oui</label> <input type="radio" name="jeux" value="FALSE" id="non"/><label for="non">non</label> <br />
 
 		<input type="submit" name="validation" />
 			</fieldset>
