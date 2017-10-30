554db8c39c536d29e7f7113dfd8d64ccae07061b
louis.bruge <louis.bruge@gmail.com>
2017-10-30 17:02:59 +0000

    Add some features to the function anniversaire_mois() (links to contact's fiche)

--- Annuaire/anniversaire.php
+++ Annuaire/anniversaire.php
@@ -5,17 +5,18 @@ function anniversaire_mois()
 	require($_SERVER['DOCUMENT_ROOT'] . '/Login/ConnectionDB.php');
 	$db = ConnectionDB();
 
-	$reponse = $db->query("SELECT nom, prenom, naissance FROM contact WHERE date_part('month', naissance) = date_part('month', current_date);");
+	$reponse = $db->query("SELECT id, nom, prenom, naissance, EXTRACT(YEAR FROM age(naissance)) AS age FROM contact WHERE date_part('month', naissance) = date_part('month', current_date);");
 
 	echo '<br />';
 	echo '<h2>Anniversaire(s) du mois</h2>';
 
-	echo '<table> <tr> <th> Contact </th> <th> Anniv </th> </tr>';
+	echo '<table> <tr> <th> Contact </th> <th> Anniv </th> <th> Age </th> </tr>';
 
 	while($donnee = $reponse->fetch())
 	{
-		echo '<tr><td>' . $donnee['prenom'] . ' ' . $donnee['nom'] . '</td>';
-		echo '<td>' . $donnee['naissance'] . '</td></tr>';
+		echo '<tr><td><a href="/Annuaire/annuaire.php?id=' . $donnee['id'] . '">' . $donnee['prenom'] . ' ' . $donnee['nom'] . '</a></td>';
+        echo '<td> Né le : ' . $donnee['naissance'] . '</td>';
+        echo '<td>' . $donnee['age'] . ' ans</td></tr>';
 	}
 
 	echo '</table>';
