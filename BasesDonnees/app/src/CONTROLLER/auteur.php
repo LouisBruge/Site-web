b9bffda90d11e21379a89a81f53e31f6b7ac0f34
griselangue <louis.bruge@gmail.com>
2017-12-10 14:52:16 +0100

    Creation de la class auteur et auteurManager pour une évolution vers une architecture POO

---
+++ BasesDonnees/app/src/CONTROLLER/auteur.php
@@ -0,0 +1,66 @@
+<?php
+
+class Auteur
+{
+	//variables privées
+	private $_id;
+	private $_nom;
+	private $_prenom;
+
+	// affiche les paramètres de la class Auteur
+	public function id()
+	{
+		return $this->_id;
+	}
+
+	public function nom()
+	{
+		return $this->_nom;
+	}
+
+	public function prenom()
+	{
+		return $this->_prenom;
+	}
+
+	// fonction d'initialisation de la class Auteur
+	public function __construct(array $donnee)
+	{
+		$this -> hydrate($donnee);
+	}
+
+
+	// fonction d'hydratation de la class Auteur
+	public function hydrate(array $donnee)
+	{
+		echo 'Appel à la mèthode hydrate';
+		foreach($donnee as $key => $value)
+		{
+			$method = 'set'.ucfirst($key);
+			if( method_exists($this, $method))
+			{
+				$this->$method($value);
+				echo 'La variable ' . $key . ' = ' . $value . '\n';
+			}
+		
+		}
+	}
+
+
+	// permet de manipuler les variables
+	public function setId($id)
+	{
+		$this->_id = (int) $id;
+	}
+
+	public function setNom($nom)
+	{
+		$this->_nom = $nom;
+	}
+
+	public function setPrenom($prenom)
+	{
+		$this->_prenom = $prenom;
+	}
+}
+?>
---
+++ BasesDonnees/app/src/modele/auteurManager.php
@@ -0,0 +1,78 @@
+<?php
+class AuteurManager 
+{
+
+	private $_db;
+
+	public function __construct($db)
+	{
+		echo 'Initialisation de la classe auteur';
+		$this->setDB($db);
+	}
+
+	public function create(Auteur $auteur)
+	{
+		echo 'Début de l\'enregistrement';
+
+		try{
+			$q = $this->_db->prepare('INSERT INTO auteur(nom, prenom) VALUES(:nom, :prenom);');
+
+			$q->bindValue(':nom', $auteur->nom());
+			$q->bindValue(':prenom', $auteur->prenom());
+
+			$q->execute();
+		}
+		catch (Exception $e)
+		{
+			$e -> getMessages();
+		}
+		echo 'Fin de l\'enregistrement';
+	}
+
+	public function update(Auteur $auteur)
+	{
+		$q = $this->_db->prepare('UPDATE auteur SET nom = :nom, prenom = :prenom WHERE id = :id');
+
+		$q->bindValue(':nom', $auteur->nom());
+		$q->bindValue(':prenom', $auteur->prenom());
+		$q->bindValue(':id', $auteur->id(), PDO::PARAM_INT);
+
+		$q->execute();
+	}
+
+	public function destroy(Auteur $auteur)
+	{
+		$this->_db->exec('DELET FROM auteur WHERE id = '.$perso->id());
+	}
+
+	public function getlist()
+	{
+		$persos = [];
+
+		$q = $this->_db->query('SELECT nom, prenom FROM auteur ORDER BY nom, prenom;');
+
+		while($donnees = $q->fetch(PDO::FETCH_ASSOC));
+		{
+			$persos = new Auteur($donnees);
+		}
+
+		return $persos;
+	}
+
+	public function get($id)
+	{
+		$id = (int) $id;
+
+		$q = $this->_db->query('SELECT nom, prenom FROM auteur WHERE id = ' . $id);
+
+		$donnees = $q->fetch(PDO::FETCH_ASSOC);
+
+		return new Auteur($donnees);
+	}
+
+	public function setDB( PDO $db)
+	{
+		$this->_db = $db;
+	}
+}
+?>
