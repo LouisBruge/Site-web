dff6f96ef7a8d432e145ffcc56e77a637a933e4b
griselangue <louis.bruge@gmail.com>
2017-12-10 16:39:44 +0100

    Transfert du formulaire citation dans la section app/view/form de la BasesDonnees

---
+++ BasesDonnees/app/view/form/citation.php
@@ -0,0 +1,18 @@
+	<form method="post" action="blabla.php" id="citation">
+	<fieldset>
+	<legend> Citation </legend>
+	Citation : <br />
+		<textarea name="citation" rows="2" cols="45"> </textarea> <br />
+	Auteur(s) : <br />
+		<input type = "text" name="auteur" required/> <br />
+	Référence(s) : <br />
+		<input type = "text" name="citation" /> <br />
+	Date scalaire : <br />
+		<input type = "number" name="date_scalaire" /> <br />
+	Siècle : <br />
+		<input type = "number" name="siecle" /> <br />
+	Date non scalaire : <br />	
+		<input type = "text" name="citation" /> <br />
+	<input type="submit" name="validation" />
+	</fieldset>
+	</form>
