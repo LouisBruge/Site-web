5563deb4c26cfec8f43f0bcb96ac2a73cd06dc1e
louis.bruge <louis.bruge@gmail.com>
2017-12-15 19:13:12 +0000

    initialisation de la class film

--- BasesDonnees/app/src/CONTROLLER/film.php
+++ BasesDonnees/app/src/CONTROLLER/film.php
@@ -1,18 +1,76 @@
 <?php
 
-	function ControllerFilm($donnee)
-	{
-		require($_SERVER['DOCUMENT_ROOT'] . '/BasesDonnees/CONTROLLER/generique.php');
-		$donnee['titre'] = ControllerTitre($donnee['titre']);
-		$donnee['realisateur'] = ControllerText($donnee['realisateur'], 'réalisateur');
-		$donnee['studio'] = ControllerText($donnee['studio'], 'studio');
-		$donnee['annee'] = ControllerInt($donnee['annee'], 'date');
-		$donnee['duree'] = ControllerInt($donnee['duree'], 'durée');
-		$donnee['commentaire'] = htmlspecialchars($donnee['commentaire']);
+   class film
+   {
+       private $id,
+           $_titre,
+           $_realisateur,
+           $_studio,
+           $_date,
+           $_duree,
+           $_commentaire;
 
-		return $donnee;
+       public functon __construct(array $donnee)
+           {
+               $this->hydrate($donnee);
+           }
 
-	}
+       public function hydrate(array $donnee)
+       {
+           foreach($donnee as $key => $value)
+           {
+            $method = 'set'.ucfirst($key);
+			    if( method_exists($this, $method))
+			    {
+				    $this->$method($value);
+				    echo 'La variable ' . $key . ' = ' . $value . '\n';
+			    }
+           }
+       }
+       
+       public function id() {    return $this->_id;   }
+       public function titre() {    return $this->_titre;   }
+       public function realisateur() {    return $this->_realisateur;   }
+       public function studio() {    return $this->_studio;   }
+       public function date() {    return $this->_date;   }
+       public function duree() {    return $this->_duree;   }
+       public function commentaire() {    return $this->_commentaire;   }
+
+       public function id($id)
+       {
+           $this-> _id = (int) $id;
+       }
+
+       public function setTitre($titre)
+       {
+           $this->_titre = $titre;
+       }
+
+       public function setRealisateur($realisateur)
+       {
+           $this->_realisateur = $realisateur;
+       }
+
+       public function setStudio($studio)
+       {
+           $this->_studio = $studio;
+       }
+
+       public function setDate($date)
+       {
+           $this->_date = (int) $date;
+       }
+
+       public function setDuree($duree)
+       {
+           $this->_duree = (int) $duree;
+       }
+
+       publid function setCommentaire($commentaire)
+       {
+           $this->_commentaire = $commentaire;
+       }
+   }
 ?>
 
 
