12c8a89932f408720bef404d90d6de0e8ea9b0a4
louis.bruge <louis.bruge@gmail.com>
2017-12-15 20:15:04 +0000

    initialisation de la class genre

--- BasesDonnees/app/src/CONTROLLER/genre.php
+++ BasesDonnees/app/src/CONTROLLER/genre.php
@@ -1,51 +1,71 @@
 <?php
-
-	function ControllerGenre($donnee)
-	{
-		$donnee['nom'] = ControllerNom($donnee['nom']);
-		$donnee['abreviation'] = ControllerAbrev($donnee['abreviation']);
-		$donnee['film'] = ControllerBoolean($donnee['film'], 'film');
-		$donnee['ouvrage'] = ControllerBoolean($donnee['ouvrage'], 'ouvrage');
-		$donnee['jeux'] = ControllerBoolean($donnee['jeux'], 'jeux');
-
-		return $donnee;
-	}
+    class genre
+    {
+        private $_id,
+            $_nom,
+            $_abreviation,
+            $_film,
+            $_ouvrage,
+            $_jeux,
 
 
-	function ControllerNom($donnee)
-	{
-		if(isset($donnee) AND preg_match('#^[a-zA-Z -]+$#', $donnee))
-		{
-			return $donnee;
-		}
-		else
-		{
-			die('Erreur dans la déclaration du nom');
-		}
-	}
+            public function id() {  return $this->_id   }
+            public function nom() {  return $this->_nom   }
+            public function abreviation() {  return $this->_abreviation   }
+            public function film() {  return $this->_film   }
+            public function ouvrage() {  return $this->_ouvrage   }
+            public function jeux() {  return $this->_jeux   }
 
-	function ControllerAbrev($donnee)
-	{
-		if(isset($donnee) AND preg_match('#^[a-zA-Z -]{1,20}$#', $donnee))
-		{
-			return $donnee;
-		}
-		else
-		{
-			die('Erreur dans la déclaration du nom');
-		}
-	}
-	function ControllerBoolean($donnee, $type)
-	{
-		if(isset($donnee) AND preg_match('#^(TRUE|FALSE)$#', $donnee))
-		{
-			$donnee = (bool) $donnee;
-			return $donnee;
-		}
-		else
-		{
-			die('Erreur dans la déclaration du type booléean du ' . $type);
-		}
-	}
-?>
+            public __construct (array $donnee)
+            {
+                $this->hydrate($donnee);
+            }
+
+    	// fonction d'hydratation de la class Auteur
+    	public function hydrate(array $donnee)
+    	{
+	    	foreach($donnee as $key => $value)
+	    	{
+		    	$method = 'set'.ucfirst($key);
+		    	if( method_exists($this, $method))
+		    	{
+			    	$this->$method($value);
+		    	}
+		
+	    	}
+    	}
+
+
+
+
+            public function setId($id)
+            {
+                $this->_id = (int) $id;
+            }
+
+            public function setNom($nom)
+            {
+                $this->_nom = $nom;
+            }
+
+            public function setAbreviation($abreviation)
+            {
+                $this->_abreviation = $abreviation;
+            }
+
+            public function setFilm($film)
+            {
+                $this->_film = (bool) $film;
+            }
+
+            public function setOuvrage($ouvrage)
+            {
+                $this->_ouvrage = (bool) $ouvrage;
+            }
+
+            public function setJeux($jeux)
+            {
+                $this->_jeux = (bool) $jeux;
+            }
+    }
 
