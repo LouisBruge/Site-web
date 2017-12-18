aa9df38d9623fcd0fab80a504c22f08ffc7727e6
griselangue <louis.bruge@gmail.com>
2017-12-18 18:58:18 +0100

    réaménagement de la page de login

--- Login/login.php
+++ Login/login.php
@@ -34,45 +34,6 @@ session_start(); ?>
 	Bienvenue, <?php echo $_SESSION['login']; ?>, au sein du serveur domestique <br />
 	Ce dernier est dédié aux bases de données bibliographiques, vidéoludiques et filmographiques. <br />
 	<br />
-
-	<section class="accesDB">
-                <div class="biblio">
-				    <h2>Base Bibliographique</h2>
-				    <p>
-                        <ul>
-                            <li><a href="/BasesDonnees/SQL/ouvrage.php">Base de Donnée</a><br/></li>
-				    	    <li><a href="/BasesDonnees/formulaires.php#monographie">Formulaires d'enregistrement</a><br/></li>
-                        </lu>
-				    </p>
-                </div>
-
-
-                <div class="jeuxVideo">
-				    <h2> Base Vidéoludique </h2>
-				    <p>
-                        <ul>
-					        <li><a href="/BasesDonnees/SQL/jeux_video.php">Base de Donnée</a><br/></li>
-					        <li><a href="/BasesDonnees/formulaires.php#jeux_video">Formulaires d'enregistrement</a><br /></li>
-                        </ul>
-				    </p>
-                </div>
-
-                <div class="film">
-				    <h2> Base Filmographique </h2>
-				    <p>
-                        <ul>
-					        <li><a href="/BasesDonnees/SQL/film.php">Base de Donnée</a><br/></li>
-    					    <li><a href="/BasesDonnees/formulaires.php#film">Formulaires d'enregistrement</a><br /></li>
-                        </ul>
-				    </p>
-                </div>
-                </section>
-		<section>
-
-	<?php 
-		require($_SERVER['DOCUMENT_ROOT'] . '/Annuaire/anniversaire.php');
-			anniversaire_mois();
-	?>
 	Pour se déconnecter, merci de cliquer sur ce <a href="deconnexion.php"> lien </a> <br />
 		</section>
 	</p>
