f1b9a14deb86d3ede67da2e93e0eebc735189d41
griselangue <louis.bruge@gmail.com>
2017-12-20 21:42:18 +0100

    ajustement des interlignes des h2 et de la hauteur des cite

--- archeo/app/view/notes/_list.php
+++ archeo/app/view/notes/_list.php
@@ -1,11 +1,9 @@
 <h2> Notes </h2>
-
 <p>
 <?php
 foreach($listnotes as $notes)
 {
 ?>
-<br />
 	<?=$notes->text();?> <br />
 	<cite> 
 	<?= $notes->source(); ?> <?= $notes->web(); ?> (consulté le <?= $notes->dateAjout() ?> )
